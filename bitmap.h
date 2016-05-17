6521c9bae77cd6943b986d2937751d1a3970fc1f
Hugh Wang <hghwng@gmail.com>
2016-05-17 20:36:26 +0800

    Fix bugs in bitmap.

--- bitmap.h
+++ bitmap.h
@@ -31,29 +31,24 @@ static inline bool bitmap_test(struct bitmap *map, size_t index) {
 
 static inline size_t bitmap_find_one(struct bitmap *map, size_t num_elements) {
   size_t num_blocks = num_elements / 64;
+  if (num_elements % 64) map->block[num_blocks++] &= (1ull << (num_elements % 64)) - 1;
+
   for (size_t i = 0; i < num_blocks; ++i) {
     if (!map->block[i]) continue;
     size_t pos = __builtin_ctzll(map->block[i]);
     return i * 64 + pos;
   }
 
-  for (size_t i = 64 * num_blocks; i < num_elements; ++i) {
-    if (bitmap_test(map, i)) return i;
-  }
-
   return -1;
 }
 
 static inline size_t bitmap_count(struct bitmap *map, size_t num_elements) {
-  size_t ret = 0;
-
   size_t num_blocks = num_elements / 64;
+  if (num_elements % 64) map->block[num_blocks++] &= (1ull << (num_elements % 64)) - 1;
+
+  size_t ret = 0;
   for (size_t i = 0; i < num_blocks; ++i) {
     ret += __builtin_popcountll(map->block[i]);
-  }
-
-  for (size_t i = 64 * num_blocks; i < num_elements; ++i) {
-    ret += bitmap_test(map, i);
   }
 
   return ret;
